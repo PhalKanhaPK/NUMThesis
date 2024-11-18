dc0fe45653232d5fc70a84f2a2f6c2642ea20f22
sary <saryr.r22@gmail.com>
2024-11-18 17:53:36 +0700

    edit file commit

--- app/src/main/java/com/example/testfirebase/HomeFragment.kt
+++ app/src/main/java/com/example/testfirebase/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.numthesis
+package com.example.testfirebase
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/example/testfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/testfirebase/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.numthesis
+package com.example.testfirebase
 
 import android.content.Intent
 import android.os.Bundle
